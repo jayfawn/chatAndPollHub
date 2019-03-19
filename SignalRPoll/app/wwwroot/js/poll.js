e00b877253fb523ac96b13b1b4786fd16f4cb99c
John Haythorn <johnhaythorn@yahoo.co.uk>
2019-03-19 13:28:58 +0000

    coomit to github for dockerfile access

---
+++ SignalRPoll/app/wwwroot/js/poll.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/pollHub").build();
+var chartBlock = '\u25A3'; //(U+25A3) is "▣" 
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = ""; //document.getElementById("messageInput").value;
+    //var myResponse = document.getElementById("myResponse").value;
+
+    if (!user) {
+        user = "[anonymous]";
+    }
+
+    if ($('input:radio[name=myResponse]').is(':checked')) {
+        var myResponseId = $('input[name=myResponse]:checked').attr('id');
+        var myResponseVal = $('input[name=myResponse]:checked').val();
+        connection.invoke("SendMessage", user, message, myResponseId, myResponseVal).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else {
+        return console.log("No response selected.");
+    }
+
+    event.preventDefault();
+});
+
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
